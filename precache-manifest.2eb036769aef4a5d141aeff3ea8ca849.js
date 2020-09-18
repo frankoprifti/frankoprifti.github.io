41db439c09f2880d85bf2a73a64ca6e1787b3e18
frankoprifti <frankoprifti@gmail.com>
2020-09-18 15:13:54 +0200

    WIP

---
+++ precache-manifest.2eb036769aef4a5d141aeff3ea8ca849.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700f096ef4ee22cab4eb246973f65401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92a4dc3a3290af188e8c",
+    "url": "/static/css/main.be1133cf.chunk.css"
+  },
+  {
+    "revision": "8ee1d86a12013a118bb6",
+    "url": "/static/js/2.64f426ec.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/static/js/2.64f426ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a4dc3a3290af188e8c",
+    "url": "/static/js/main.a2ac85a5.chunk.js"
+  },
+  {
+    "revision": "2903eb1f4c21d3b2e2e5",
+    "url": "/static/js/runtime-main.89ba0b7d.js"
+  },
+  {
+    "revision": "ac33e93dfd91e6a172837e05e6007e76",
+    "url": "/static/media/map.ac33e93d.png"
+  },
+  {
+    "revision": "01918c9c21716b48d10e91205e9477cf",
+    "url": "/static/media/profile_photo.01918c9c.jpg"
+  },
+  {
+    "revision": "ecfcbf105d423eca79ecf475d14a4000",
+    "url": "/static/media/project1.ecfcbf10.png"
+  },
+  {
+    "revision": "ce1101a47217075b0c9505cc40d60050",
+    "url": "/static/media/project2.ce1101a4.png"
+  },
+  {
+    "revision": "ec934d7dfa454fe7bee57a1fbce7c0f4",
+    "url": "/static/media/project3.ec934d7d.png"
+  },
+  {
+    "revision": "8b1b8325edb426d0acd9b479086a28db",
+    "url": "/static/media/project4.8b1b8325.png"
+  }
+]);
