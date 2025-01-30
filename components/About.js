ed75f004a90cedc837c9f2db191adca730fffbc5
Franko Prifti <frankoprifti@gmail.com>
2025-01-30 17:26:05 +0100

    Update About.js

--- components/About.js
+++ components/About.js
@@ -149,7 +149,7 @@ const About = () => {
               </div>
               <a
                 className="button group mt-30 overflow-hidden inline-block leading-lh-1.4 rounded-30 text-ellipsis text-center align-middle select-none transition-all duration-250 ease-in-out uppercase no-underline relative z-10 py-16 pr-70 pl-35 text-fs-15 font-semibold text-white bg-transparent outline-0 before:absolute before:-z-10 before:left-0 before:right-0 before:top-0 before:bottom-0 before:translate-x-full hover:before:translate-x-0 before:transition before:duration-300 before:ease-out"
-                href="https://drive.google.com/file/d/1k3OHHJnnpPby2iwz4MsT4Zaki6YumfwN/view?usp=sharing"
+                href="https://drive.google.com/file/d/1JJRy-HOBDvu0W4WjJky45fXxCzQmEW4U/view?usp=drive_link"
                 target='_blank'
               >
                 <span
